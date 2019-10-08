f01cfbf6a3c314cfa8c5b82764ed2c43c7bd8186
Gogs <gogs@fake.local>
2019-10-08 19:01:12 +0800

    add migratiosn

--- reminder/modules/apps/oauth/models.py
+++ reminder/modules/apps/oauth/models.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 
 
 class User(AbstractUser):
-    group_name = models.CharField(max_length=128, verbose_name="Group Name")
-    group_id = models.IntegerField(verbose_name="group id")
+    group_name = models.CharField(max_length=128, default="support", verbose_name="Group Name")
+    group_id = models.IntegerField(verbose_name="group id", default=100)
     open_id = models.CharField(max_length=128, verbose_name="Wechat Open Id")
